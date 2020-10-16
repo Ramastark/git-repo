d6823d986c5ac179974a76c88d35ef145dd0956c
Ramastark <ramastark86@gmail.com>
2020-10-16 10:24:48 +0530

    added price

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -4,6 +4,22 @@ public class Product {
 	int productId;
 	String productName;
 	String productDescription;
+	int quantity;
+	double price;
+	
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	public int getProductId() {
 		return productId;
 	}
